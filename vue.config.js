a47cf4a88fc8d796cf051572b1663f3d6bb5310a
juxsta <ericjrreyes2@gmail.com>
2020-07-22 15:09:04 -0700

    added vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,5 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-param-reassign */
-const os = require('os');
-
 module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
-  chainWebpack: (config) => {
-    config
-      .plugin('fork-ts-checker')
-      .tap((args) => {
-        const totalmem = Math.floor(os.totalmem() / 1024 / 1024); // get OS mem size
-        const allowUseMem = process.env.MEMORY_LIMIT || (totalmem > 2500 ? 2048 : 1000);
-        args[0].memoryLimit = allowUseMem;
-        return args;
-      });
-  },
-  // parallel: require('os').cpus().length > 1,
 };
