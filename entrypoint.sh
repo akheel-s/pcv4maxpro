f64740a77b5c839b21d4532e9250367f03ffbab4
juxsta <ericjrreyes2@gmail.com>
2020-07-29 13:13:58 -0700

    :rotating_light: updated eslint to ignore linebreak style

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -arv --relative /usr/src/cache/node_modules/. /usr/src/app/node_modules/
+rsync -arv /usr/src/cache/node_modules/. /usr/src/app/node_modules/
 exec yarn run serve
