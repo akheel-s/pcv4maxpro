7d4dc36038c46a7d82c71d02a1d1d6d8d12dad43
Prajit Saravanan <prajit.saravana@gmail.com>
2020-08-03 13:12:52 -0700

    working on the login page

---
+++ src/components/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="login__background">
+    <div class="login__navbar">
+      <img src="@/assets/Pilotcity_logo.png" class="login__image" />
+      <span class="text-h4 login__header font-weight-black">PilotCity</span>
+      <v-btn @click="callback" class="login__button">Login</v-btn>
+      <v-btn @click="callback" class="login__signup">Signup</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
+
+<style lang="scss">
+.login{
+  &__background{
+      height: 100%;
+      background-color: #4f4f4f;
+  }
+}
+
+.login{
+  &__navbar{
+  }
+  &__header{
+    color: #f2f2f2;
+  }
+}
+
+.login__image {
+  width: 2%;
+  padding-top: 1%;
+}
+
+.login__button {
+  width: 10.5%;
+  height: 50px;
+  border: 1px solid #f2f2f2;
+  box-sizing: border-box;
+  border-radius: 25px;
+}
+
+.login__signup {
+  background: #828282;
+  border-radius: 25px;
+}
+</style>
