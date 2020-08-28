4e342e6a21b3a4a8aa14d8c3c3eeb2b60b14dbeb
juxsta <ericjrreyes2@gmail.com>
2020-08-28 12:18:21 -0700

    :bug: linted and updated resetpassword

--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -1,20 +1,20 @@
 <template>
-  <ValidationObserver v-slot="{ invalid, valid }" class="signup_inputs">
-    <validation-provider v-slot="{ errors }" name="confirm" rules="required">
+  <ValidationObserver v-slot="{ invalid }" class="signup_inputs">
+    <validation-provider v-slot="{ errors }" name="confirm" rules="required|min:6">
       <div class="signup__confirmpassword text-subtitle-2">New Password</div>
 
-      <Password
+      <v-text-field
         v-model="password"
         color="white"
         label="Password"
         class="signup__input"
+        :error-messages="errors"
         single-line
         outlined
         full-width
         toggle
         @score="handleScore"
-      ></Password>
-      <span>{{ errors[0] }}</span>
+      ></v-text-field>
       <span v-if="valid && score < 3">Your password is not strong enough</span>
     </validation-provider>
 
@@ -22,24 +22,19 @@
       class="signup__signupbuttons text-h6 font-weight-black"
       depressed
       color="green"
-      :disabled="invalid || score < 3"
+      :disabled="invalid"
+      :loading="loading"
       @click="submit"
       >Change Password</v-btn
     >
-    <v-alert v-if="success || error" :type="success ? 'success' : 'error'">{{
-      success || error
-    }}</v-alert>
+    <v-alert v-if="msg" :type="type">{{ msg }}</v-alert>
   </ValidationObserver>
 </template>
 <script lang="ts">
-import Password from 'vue-password-strength-meter';
-import { reactive, toRefs, ref } from '@vue/composition-api';
+import { reactive, toRefs } from '@vue/composition-api';
 import { useActions } from '@/store/modules/auth';
 
 export default {
-  components: {
-    Password
-  },
   props: {
     token: {
       type: String,
@@ -54,28 +49,29 @@ export default {
     // reset password logic
     const state = reactive({
       password: '',
-      success: '',
-      error: ''
+      msg: '',
+      type: 'success',
+      loading: false
     });
     const { resetPassword } = useActions(['resetPassword']);
     async function submit() {
+      state.loading = true;
       try {
         await resetPassword({
           token: props.token as string,
           tokenId: props.tokenId as string,
           password: state.password
         });
-        state.success = 'Your password has been changed';
-      } catch {
-        state.error = 'Could not change your password';
+        state.msg = 'Your password has been changed';
+        state.type = 'success';
+      } catch (err) {
+        state.msg = err;
+        state.type = 'error';
       }
+      state.loading = false;
     }
     // validation logic
-    const score = ref(0);
-    function handleScore(newScore: number) {
-      score.value = newScore;
-    }
-    return { ...toRefs(state), submit, handleScore, score };
+    return { ...toRefs(state), submit };
   }
 };
 </script>
