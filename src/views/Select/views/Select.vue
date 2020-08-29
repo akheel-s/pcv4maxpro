1664ab9654e735db6b8caff9aaafb01cedd1aa8a
Prajit Saravanan <prajit.saravana@gmail.com>
2020-08-29 15:49:30 -0700

    🎨 need to work on improving the stepper

--- src/views/Select/views/Select.vue
+++ src/views/Select/views/Select.vue
@@ -2,15 +2,15 @@
   <div class="select__entire-body">
     <v-stepper class="select__header" alt-labels>
       <v-stepper-header>
-        <v-stepper-step class="select__stepone" step="1">Select</v-stepper-step>
+        <v-stepper-step class="select__stepone" step="">Select</v-stepper-step>
 
-        <v-divider></v-divider>
+        <v-divider class="select__divone"></v-divider>
 
-        <v-stepper-step class="select__steptwo" step="2"> Details </v-stepper-step>
+        <v-stepper-step class="select__steptwo" step=""> Details </v-stepper-step>
 
-        <v-divider></v-divider>
+        <v-divider c></v-divider>
 
-        <v-stepper-step class="select__stepthree" step="3">Manage</v-stepper-step>
+        <v-stepper-step class="select__stepthree" step="">Manage</v-stepper-step>
       </v-stepper-header>
     </v-stepper>
   </div>
@@ -32,6 +32,35 @@ export default {
     &.v-stepper {
       box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.2), 0px 0px 0px 0px rgba(0, 0, 0, 0.14),
         0px 0px 0px 0px rgba(0, 0, 0, 0.12);
+    }
+  }
+
+  &__stepone {
+    & .v-stepper__step__step {
+      width: 100px;
+      height: 100px;
+      height: 100px;
+      border: 3px solid #e0e0e0;
+    }
+  }
+
+  &__divone {
+    &.v-divider {
+    }
+  }
+  &__steptwo {
+    & .v-stepper__step__step {
+      width: 100px;
+      height: 100px;
+      border: 3px dashed #e0e0e0;
+    }
+  }
+
+  &__stepthree {
+    & .v-stepper__step__step {
+      width: 100px;
+      height: 100px;
+      border: 3px dashed #e0e0e0;
     }
   }
 }
