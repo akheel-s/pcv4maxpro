d178c1d56128c533166c388b5a3ab0111f4e3d63
Prajit Saravanan <prajit.saravana@gmail.com>
2020-08-29 03:04:23 -0700

    💄 Removed the elevation of the stepper function

--- src/views/Select/views/Select.vue
+++ src/views/Select/views/Select.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="select__entire-body">
-    <v-stepper alt-labels>
+    <v-stepper class="select__header" alt-labels>
       <v-stepper-header>
-        <v-stepper-step step="1">Ad unit details</v-stepper-step>
+        <v-stepper-step class="select__stepone" step="1">Select</v-stepper-step>
 
         <v-divider></v-divider>
 
-        <v-stepper-step step="2">Ad sizes</v-stepper-step>
+        <v-stepper-step class="select__steptwo" step="2"> Details </v-stepper-step>
 
         <v-divider></v-divider>
 
-        <v-stepper-step step="3">Ad templates</v-stepper-step>
+        <v-stepper-step class="select__stepthree" step="3">Manage</v-stepper-step>
       </v-stepper-header>
     </v-stepper>
   </div>
@@ -26,6 +26,13 @@ export default {
 .select {
   &__entire-body {
     height: 100%;
+  }
+
+  &__header {
+    &.v-stepper {
+      box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.2), 0px 0px 0px 0px rgba(0, 0, 0, 0.14),
+        0px 0px 0px 0px rgba(0, 0, 0, 0.12);
+    }
   }
 }
 </style>
