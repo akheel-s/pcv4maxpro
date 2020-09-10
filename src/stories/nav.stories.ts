0fe5002f08ee4212f7e8c818453ad6f6988caa09
Prajit Saravanan <prajit.saravana@gmail.com>
2020-09-09 18:52:48 -0700

    💄 STORYBOOK WORKED

--- src/stories/nav.stories.ts
+++ src/stories/nav.stories.ts
@@ -1,6 +1,7 @@
 import VueCompositionAPI from '@vue/composition-api';
 import Nav from '@/views/ManageProgram/components/Nav.vue';
 import { withKnobs, text } from '@storybook/addon-knobs';
+import { Meta, Story } from '@/@types/storybook.d';
 import Vue from 'vue';
 
 Vue.use(VueCompositionAPI);
@@ -9,21 +10,24 @@ export default {
   component: Nav,
   title: 'Manage Program Sidebar',
   decorators: [withKnobs]
-};
+} as Meta;
 
-export const asDefault = () => ({
-  components: {
-    Nav
-  },
-  template: '<Nav :startDate="startDate" :endDate="endDate"></Nav>',
-  props: {
-    startDate: {
-      default: text('startDate', 'DAY, MON 1'),
-       
-    },
-    endDate: {
-      default: text('endDate', 'DAY, MON 1'),
-       
-    }
-  }
+const Template = (_args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Nav },
+  template: '<Nav :startDate="startDate" :endDate="endDate"></Nav>'
 });
+
+export const asDefault: Story = Template.bind({});
+  
+asDefault.args = {      
+ startDate: text('startDate', 'DAY, MON 1'),
+ endDate:  text('startDate', 'DAY, MON 1'),
+}
+       
+
+
+
+
+
+
