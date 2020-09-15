7f015baf0c5249b726b6f4b865002df3c7bb21fa
Derick Lee <dericklee@pilotcity.com>
2020-09-15 01:11:33 -0700

    :lipstick: made some styline changes to setup program

--- src/views/SetupProgram/setupprogram.vue
+++ src/views/SetupProgram/setupprogram.vue
@@ -16,7 +16,7 @@
           </v-stepper-header>
         </v-stepper>
       </div>
-      <div class="setupprogram__title text-h4 font-weight-black">
+      <div class="setupprogram__title">
         Setup Program
       </div>
       <div class="setupprogram__uploadbox text-center" color="gray">
@@ -24,67 +24,69 @@
           <v-icon x-large="5">mdi-arrow-up-bold-circle</v-icon>
         </v-btn>
       </div>
-      <div class="setupprogram__pname font-weight-black">Program Name</div>
+      <div class="setupprogram__pname font-weight-bold">Program Title</div>
       <div class="setupprogram__pinput">
-        <v-text-field label="Program Name" single-line outlined dense></v-text-field>
+        <v-text-field hide-details label="Program Title" single-line outlined dense></v-text-field>
       </div>
-      <div class="setupprogram__description font-weight-black">Description</div>
+      <div class="setupprogram__description font-weight-bold">Description</div>
       <div class="setupprogram__dinput">
-        <v-textarea label="Description" full-width single-line outlined></v-textarea>
+        <v-textarea hide-details label="Description" full-width single-line outlined></v-textarea>
       </div>
       <div class="setupprogram__calendartitles">
-        <div class="setupprogram__frequency font-weight-black">Frequency</div>
-        <div class="setupprogram__startdate font-weight-black">Start Date</div>
-        <div class="setupprogram__enddate font-weight-black">End Date</div>
+        <div class="setupprogram__frequency font-weight-bold">Frequency</div>
+        <div class="setupprogram__startdate font-weight-bold">Start Date</div>
+        <div class="setupprogram__enddate font-weight-bold">End Date</div>
       </div>
       <div class="setupprogram__calendarinputs">
         <div class="setupprogram__finput">
-          <v-combobox v-model="select" :items="items" outlined dense></v-combobox>
+          <v-combobox v-model="select" hide-details :items="items" outlined dense></v-combobox>
         </div>
         <div class="setupprogram__cinput">
-          <v-combobox v-model="select" :items="items" outlined dense></v-combobox>
+          <v-combobox v-model="select" hide-details :items="items" outlined dense></v-combobox>
         </div>
         <div class="setupprogram__cinput1">
-          <v-combobox v-model="select" :items="items" outlined dense></v-combobox>
+          <v-combobox v-model="select" hide-details :items="items" outlined dense></v-combobox>
         </div>
       </div>
 
-      <div class="setupprogram__oname font-weight-black">Organizer Name</div>
+      <div class="setupprogram__oname font-weight-bold">Organizer Name</div>
       <div class="setupprogram__oinput">
-        <v-text-field label="Organizer Name" single-line outlined dense></v-text-field>
+        <v-text-field hide-details label="Organizer Name" single-line outlined dense></v-text-field>
       </div>
-      <div class="setupprogram__olocation font-weight-black">Organizer Location</div>
+      <div class="setupprogram__olocation font-weight-bold">Organizer Location</div>
       <div class="setupprogram__olinput">
-        <v-text-field label="Organizer Location" single-line outlined dense></v-text-field>
+        <v-text-field
+          hide-details
+          label="Organizer Location"
+          single-line
+          outlined
+          dense
+        ></v-text-field>
       </div>
 
       <div class="setupprogram__qualification">
-        <div class="setupprogram__q1 font-weight-black">Qualification #1</div>
+        <div class="setupprogram__q1 font-weight-bold">Qualification #1</div>
         <div class="setupprogram__q1input">
-          <v-combobox v-model="select" :items="items" outlined dense></v-combobox>
+          <v-combobox v-model="select" hide-details :items="items" outlined dense></v-combobox>
         </div>
       </div>
 
       <div class="setupprogram__qualification2">
-        <div class="setupprogram__q2 font-weight-black">Qualification #2</div>
+        <div class="setupprogram__q2 font-weight-bold">Qualification #2</div>
         <div class="setupprogram__q2input">
-          <v-combobox v-model="select" :items="items" outlined dense></v-combobox>
+          <v-combobox v-model="select" hide-details :items="items" outlined dense></v-combobox>
         </div>
       </div>
 
       <div class="setupprogram__qualification3">
-        <div class="setupprogram__q3 font-weight-black">Qualification #3</div>
+        <div class="setupprogram__q3 font-weight-bold">Qualification #3</div>
         <div class="setupprogram__q3input">
-          <v-combobox v-model="select" :items="items" outlined dense></v-combobox>
+          <v-combobox v-model="select" hide-details :items="items" outlined dense></v-combobox>
         </div>
       </div>
 
       <div class="setupprogram__complete">
-        <v-btn
-          class="setupprogram__button text-h5 text-center font-weight-black"
-          depressed
-          outlined
-        >
+        <v-btn class="setupprogram__button text-center font-weight-bold" depressed outlined>
           Complete
         </v-btn>
       </div>
@@ -103,8 +105,11 @@ export default {
 <style lang="scss">
 .setupprogram {
   &__title {
-    margin-top: 4.41%;
-    margin-left: 3.88%;
+    margin-top: 18%;
+    margin-left: 2%;
+    font-family: Raleway;
+    font-size: 36px;
+    font-weight: 800;
   }
 }
 
@@ -121,6 +126,7 @@ export default {
     margin-right: 3.88%;
     max-width: 550px;
     margin-top: 8.3%;
+    font-family: Raleway;
   }
 
   &__uploadbox {
@@ -128,9 +134,10 @@ export default {
     width: 100%;
     height: 100%;
     background: #f2f2f2;
-    border: 1px dashed #000000;
+    border: 1px solid #bcbec0;
     box-sizing: border-box;
     border-radius: 10px;
+    font-family: Raleway;
   }
 
   &__icon {
@@ -138,143 +145,185 @@ export default {
     height: 100px;
     margin-bottom: 20%;
     margin-top: 20%;
-
+    font-family: Raleway;
     background: #c4c4c4;
   }
 
   &__pname {
-    margin-top: 3.73%;
-    margin-bottom: 1.9%;
-    margin-left: 2%;
+    margin-top: 2.5%;
+    margin-bottom: 2.5%;
+    margin-left: 1%;
+    font-family: Raleway;
   }
 
   &__pinput {
     max-width: 550px;
+    font-family: Raleway;
   }
 
   &__description {
-    margin-bottom: 1.9%;
-    margin-left: 2%;
+    margin-top: 2.5%;
+    margin-bottom: 2.5%;
+    margin-left: 1%;
+    font-family: Raleway;
   }
 
   &__dinput {
-    margin-top: 0.1%;
     max-width: 550px;
+    font-family: Raleway;
   }
 
   &__calendartitles {
     display: flex;
     flex-direction: row;
     max-width: 550px;
-    margin-bottom: 2%;
+    font-family: Raleway;
   }
 
   &__frequency {
-    margin-left: 2%;
-    margin-right: 19%;
+    // margin-left: 1%;
+    // margin-right: 19%;
+    font-family: Raleway;
+    margin-top: 2.5%;
+    margin-bottom: 2.5%;
+    width: 33%;
+    margin-left: 1%;
   }
 
   &__startdate {
-    margin-right: 18%;
+    // margin-right: 18%;
+    font-family: Raleway;
+    margin-top: 2.5%;
+    margin-bottom: 2.5%;
+    width: 33%;
+    margin-left: 1%;
   }
 
   &__enddate {
-    margin-left: 0%;
+    // margin-left: 0%;
+    font-family: Raleway;
+    margin-top: 2.5%;
+    margin-bottom: 2.5%;
+    width: 33%;
+    margin-left: 1%;
   }
 
   &__calendarinputs {
     display: flex;
     flex-direction: row;
+    font-family: Raleway;
   }
 
   &__finput {
-    max-width: 172px;
+    max-width: 33%;
+    font-family: Raleway;
   }
 
   &__cinput {
-    max-width: 172px;
-    margin-left: 1.18%;
+    max-width: 33%;
+    margin-left: 1%;
+    font-family: Raleway;
   }
 
   &__cinput1 {
-    max-width: 172px;
-    margin-left: 1.18%;
+    max-width: 33%;
+    margin-left: 1%;
+    font-family: Raleway;
   }
 
   &__oname {
-    margin-bottom: 1.9%;
-    margin-left: 2%;
+    margin-left: 1%;
+    font-family: Raleway;
+    margin-top: 2.5%;
+    margin-bottom: 2.5%;
   }
 
   &__oinput {
     max-width: 550px;
+    font-family: Raleway;
   }
 
   &__olocation {
-    margin-bottom: 1.9%;
-    margin-left: 2%;
+    margin-left: 1%;
+    font-family: Raleway;
+    margin-top: 2.5%;
+    margin-bottom: 2.5%;
   }
 
   &__olinput {
     max-width: 550px;
+    font-family: Raleway;
   }
 
   &__qualification {
     display: flex;
     flex-direction: row;
     max-width: 550px;
+    font-family: Raleway;
   }
 
   &__q1 {
-    margin-left: 0.5%;
-    margin-right: 18.5%;
-    padding-top: 1%;
+    // margin-left: 0.5%;
+    margin-right: 17.5%;
+    padding-top: 6.5%;
+    font-family: Raleway;
   }
 
   &__q1input {
     width: 60%;
     max-width: 70%;
+    font-family: Raleway;
+    margin-top: 5%;
   }
 
   &__qualification2 {
     display: flex;
     flex-direction: row;
     max-width: 550px;
+    font-family: Raleway;
   }
 
   &__q2 {
-    margin-left: 0.5%;
-    margin-right: 18.5%;
-    padding-top: 1%;
+    // margin-left: 0.5%;
+    margin-right: 17.5%;
+    padding-top: 6.5%;
+    font-family: Raleway;
   }
 
   &__q2input {
     width: 60%;
     max-width: 70%;
+    font-family: Raleway;
+    margin-top: 5%;
   }
 
   &__qualification3 {
     display: flex;
     flex-direction: row;
     max-width: 550px;
+    font-family: Raleway;
   }
 
   &__q3 {
-    margin-left: 0.5%;
-    margin-right: 18.5%;
-    padding-top: 1%;
+    // margin-left: 0.5%;
+    margin-right: 17.5%;
+    padding-top: 6.5%;
   }
 
   &__q3input {
     width: 60%;
     max-width: 70%;
+    font-family: Raleway;
+    margin-top: 5%;
   }
 
   &__button {
     justify-content: center;
     max-width: 550px;
     width: 100%;
-    margin-bottom: 5%;
+    margin-top: 10%;
+    margin-bottom: 20%;
+    font-family: Raleway;
   }
 }
 </style>
