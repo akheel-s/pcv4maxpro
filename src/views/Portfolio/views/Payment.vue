fe567b955a334cabb96ab7a1992ba265a1487706
Prajit Saravanan <prajit.saravana@gmail.com>
2020-09-23 18:01:07 -0700

    Added validation and then changed teh referral page

--- src/views/Portfolio/views/Payment.vue
+++ src/views/Portfolio/views/Payment.vue
@@ -1,179 +1,188 @@
 <template>
-  <div class="sponsor__container">
-    <div class="sponsor__details">
-      <div class="sponsor__details-avatar">
-        <v-avatar size="60">
-          <v-img
-            src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/91356050_3160034130674652_4990180745826795520_o.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=wHg8nkrEmDAAX_l8bBN&_nc_ht=scontent-sjc3-1.xx&oh=2280183a7bf702fd605883a9dacd3984&oe=5F75E2E0"
-          ></v-img>
-        </v-avatar>
+  <ValidationObserver v-slot="{}">
+    <div class="sponsor__container">
+      <div class="sponsor__details">
+        <div class="sponsor__details-avatar">
+          <v-avatar size="60">
+            <v-img
+              src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/91356050_3160034130674652_4990180745826795520_o.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=wHg8nkrEmDAAX_l8bBN&_nc_ht=scontent-sjc3-1.xx&oh=2280183a7bf702fd605883a9dacd3984&oe=5F75E2E0"
+            ></v-img>
+          </v-avatar>
+        </div>
+
+        <div class="sponsor__details-namecard">
+          <validation-provider v-slot="{ errors }" rules="required">
+            <v-text-field
+              v-model="sponsorName"
+              :error-messages="errors"
+              height="60"
+              outlined
+              rounded
+              label="Sponsor"
+              class="sponsor__details-name font-weight-bold"
+              hint="This will be your public display name"
+              placeholder="Jerold Inocencio"
+            ></v-text-field>
+          </validation-provider>
+
+          <v-card class="sponsor__support-card" elevation="0" ripple outlined width="100%">
+            <v-card-title>Latest transactions</v-card-title>
+
+            <v-timeline dense>
+              <v-timeline-item icon="mdi-currency-usd-circle" small fill-dot color="grey"
+                ><v-chip color="grey" small dark>$7,500</v-chip></v-timeline-item
+              >
+              <v-timeline-item icon="mdi-currency-usd-circle" small fill-dot color="grey"
+                ><v-chip color="grey" small dark>$2,250</v-chip></v-timeline-item
+              >
+              <v-timeline-item icon="mdi-currency-usd-circle" small fill-dot color="grey"
+                ><v-chip color="grey" small dark>$24,500</v-chip></v-timeline-item
+              >
+            </v-timeline>
+          </v-card>
+
+          <v-switch
+            inset
+            color="red"
+            class="sponsor__details-anonymous"
+            label="Make public display name anonymous"
+          >
+          </v-switch>
+        </div>
       </div>
-      <div class="sponsor__details-namecard">
-        <v-text-field
-          height="60"
-          outlined
-          rounded
-          label="Sponsor"
-          class="sponsor__details-name font-weight-bold"
-          hint="This will be your public display name"
-          placeholder="Jerold Inocencio"
-        ></v-text-field>
 
-        <v-card class="sponsor__support-card" elevation="0" ripple outlined width="100%">
-          <v-card-title>Latest transactions</v-card-title>
-          <!-- <v-card-subtitle>The results of your actions</v-card-subtitle> -->
-          <v-timeline dense>
-            <v-timeline-item icon="mdi-currency-usd-circle" small fill-dot color="grey"
-              ><v-chip color="grey" small dark>$7,500</v-chip></v-timeline-item
-            >
-            <v-timeline-item icon="mdi-currency-usd-circle" small fill-dot color="grey"
-              ><v-chip color="grey" small dark>$2,250</v-chip></v-timeline-item
-            >
-            <v-timeline-item icon="mdi-currency-usd-circle" small fill-dot color="grey"
-              ><v-chip color="grey" small dark>$24,500</v-chip></v-timeline-item
-            >
-          </v-timeline>
-        </v-card>
+      <div class="sponsor__timeline-container">
+        <v-stepper v-model="e6" non-linear class="sponsor__timeline pb-0" vertical>
+          <v-stepper-step
+            class="sponsor__timeline-step"
+            color="#6fba7f"
+            :complete="e6 > 1"
+            step="1"
+            non-linear
+          >
+            Select participants
+          </v-stepper-step>
 
-        <v-switch
-          inset
-          color="red"
-          class="sponsor__details-anonymous"
-          label="Make public display name anonymous"
-        >
-        </v-switch>
+          <v-stepper-content step="1">
+            <div class="sponsor__program-menu">
+              <v-card elevation="0" color="green" dark class="mb-3" width="200px" height="200px">
+                <v-card-title>Student</v-card-title>
+                <v-card-subtitle
+                  >Per Student. Get an all out pass for one participant in our
+                  programs.</v-card-subtitle
+                >
+                <v-card-actions>
+                  <v-radio-group v-model="ex1" class="sponsor__menu-radio"
+                    ><v-radio class="sponsor__menu-radio" label="$500"></v-radio></v-radio-group
+                ></v-card-actions>
+              </v-card>
+
+              <v-card elevation="0" dark color="blue" class="mb-3" width="200px" height="200px">
+                <v-card-title>Class</v-card-title>
+                <v-card-subtitle
+                  >Up to 30 students. Have your entire classroom participate in our
+                  program.</v-card-subtitle
+                >
+                <v-card-actions>
+                  <v-radio-group v-model="ex1" class="sponsor__menu-radio"
+                    ><v-radio class="sponsor__menu-radio" label="$5,250"></v-radio></v-radio-group
+                ></v-card-actions>
+              </v-card>
+              <v-card elevation="0" dark color="pink" class="mb-3" width="200px" height="200px">
+                <v-card-title>Teacher</v-card-title>
+                <v-card-subtitle
+                  >Up to 90 students. Enroll multiple classroom sections in our
+                  program.</v-card-subtitle
+                >
+                <v-card-actions>
+                  <v-radio-group v-model="ex1" class="sponsor__menu-radio"
+                    ><v-radio class="sponsor__menu-radio" label="$11,250"></v-radio></v-radio-group
+                ></v-card-actions>
+              </v-card>
+
+              <v-card
+                elevation="0"
+                color="orange"
+                dark
+                class="mb-3 sponsor__program-menu-card"
+                width="200px"
+                height="200px"
+              >
+                <v-card-title>Family</v-card-title>
+                <v-card-subtitle
+                  >Up to 3 Students. Enroll all your students in a family bundle.</v-card-subtitle
+                >
+                <v-card-actions>
+                  <v-radio-group v-model="ex1" class="sponsor__menu-radio"
+                    ><v-radio class="sponsor__menu-radio" label="$1,350"></v-radio></v-radio-group
+                ></v-card-actions>
+              </v-card>
+              <v-card elevation="0" color="purple" dark class="mb-3" width="200px" height="200px">
+                <v-card-title>Group</v-card-title>
+                <v-card-subtitle
+                  >Up to 5 Students. Great for a group or team of friends and family
+                  members.</v-card-subtitle
+                >
+                <v-card-actions>
+                  <v-radio-group v-model="ex1" class="sponsor__menu-radio"
+                    ><v-radio class="sponsor__menu-radio" label="$2,000"></v-radio></v-radio-group
+                ></v-card-actions>
+              </v-card>
+            </div>
+            <v-btn small outlined text @click="e6 = 2">Enter Payment Details</v-btn>
+            <v-btn small text>Cancel</v-btn>
+          </v-stepper-content>
+
+          <v-stepper-step
+            class="sponsor__timeline-step"
+            color="#3c9dcd"
+            :complete="e6 > 2"
+            step="2"
+            non-linear
+            >Pay for sponsorship</v-stepper-step
+          >
+
+          <v-stepper-content step="2">
+            <v-card elevation="0" color="grey lighten-3" class="mb-3" height="50px"></v-card>
+            <v-btn small outlined text @click="e6 = 3">Confirm and Complete</v-btn>
+            <v-btn small text>Cancel</v-btn>
+          </v-stepper-content>
+
+          <v-stepper-step
+            class="sponsor__timeline-step"
+            color="#ae92b1"
+            :complete="e6 > 3"
+            step="3"
+            non-linear
+            >Finish transaction</v-stepper-step
+          >
+
+          <v-stepper-content step="3">
+            <v-card elevation="0" color="grey lighten-3" class="mb-3" height="50px"></v-card>
+            <v-btn small outlined text @click="e6 = 4">Complete Transaction</v-btn>
+            <v-btn small text>Cancel</v-btn>
+          </v-stepper-content>
+        </v-stepper>
       </div>
-
-      <!-- <div>
-        <v-card draggable> Rankings</v-card>
-      </div> -->
-
-      <!-- <div class="sponsor__details-name">Jerold Inocencio</div> -->
     </div>
-
-    <div class="sponsor__timeline-container">
-      <v-stepper v-model="e6" non-linear class="sponsor__timeline pb-0" vertical>
-        <v-stepper-step
-          class="sponsor__timeline-step"
-          color="#6fba7f"
-          :complete="e6 > 1"
-          step="1"
-          non-linear
-        >
-          Select participants
-          <!-- <small>Summarize if needed</small> -->
-        </v-stepper-step>
-
-        <v-stepper-content step="1">
-          <div class="sponsor__program-menu">
-            <v-card elevation="0" color="green" dark class="mb-3" width="200px" height="200px">
-              <v-card-title>Student</v-card-title>
-              <v-card-subtitle
-                >Per Student. Get an all out pass for one participant in our
-                programs.</v-card-subtitle
-              >
-              <v-card-actions>
-                <v-radio-group v-model="ex1" class="sponsor__menu-radio"
-                  ><v-radio class="sponsor__menu-radio" label="$500"></v-radio></v-radio-group
-              ></v-card-actions>
-            </v-card>
-
-            <v-card elevation="0" dark color="blue" class="mb-3" width="200px" height="200px">
-              <v-card-title>Class</v-card-title>
-              <v-card-subtitle
-                >Up to 30 students. Have your entire classroom participate in our
-                program.</v-card-subtitle
-              >
-              <v-card-actions>
-                <v-radio-group v-model="ex1" class="sponsor__menu-radio"
-                  ><v-radio class="sponsor__menu-radio" label="$5,250"></v-radio></v-radio-group
-              ></v-card-actions>
-            </v-card>
-            <v-card elevation="0" dark color="pink" class="mb-3" width="200px" height="200px">
-              <v-card-title>Teacher</v-card-title>
-              <v-card-subtitle
-                >Up to 90 students. Enroll multiple classroom sections in our
-                program.</v-card-subtitle
-              >
-              <v-card-actions>
-                <v-radio-group v-model="ex1" class="sponsor__menu-radio"
-                  ><v-radio class="sponsor__menu-radio" label="$11,250"></v-radio></v-radio-group
-              ></v-card-actions>
-            </v-card>
-
-            <v-card
-              elevation="0"
-              color="orange"
-              dark
-              class="mb-3 sponsor__program-menu-card"
-              width="200px"
-              height="200px"
-            >
-              <v-card-title>Family</v-card-title>
-              <v-card-subtitle
-                >Up to 3 Students. Enroll all your students in a family bundle.</v-card-subtitle
-              >
-              <v-card-actions>
-                <v-radio-group v-model="ex1" class="sponsor__menu-radio"
-                  ><v-radio class="sponsor__menu-radio" label="$1,350"></v-radio></v-radio-group
-              ></v-card-actions>
-            </v-card>
-            <v-card elevation="0" color="purple" dark class="mb-3" width="200px" height="200px">
-              <v-card-title>Group</v-card-title>
-              <v-card-subtitle
-                >Up to 5 Students. Great for a group or team of friends and family
-                members.</v-card-subtitle
-              >
-              <v-card-actions>
-                <v-radio-group v-model="ex1" class="sponsor__menu-radio"
-                  ><v-radio class="sponsor__menu-radio" label="$2,000"></v-radio></v-radio-group
-              ></v-card-actions>
-            </v-card>
-          </div>
-          <v-btn small outlined text @click="e6 = 2">Enter Payment Details</v-btn>
-          <v-btn small text>Cancel</v-btn>
-        </v-stepper-content>
-
-        <v-stepper-step
-          class="sponsor__timeline-step"
-          color="#3c9dcd"
-          :complete="e6 > 2"
-          step="2"
-          non-linear
-          >Pay for sponsorship</v-stepper-step
-        >
-
-        <v-stepper-content step="2">
-          <v-card elevation="0" color="grey lighten-3" class="mb-3" height="50px"></v-card>
-          <v-btn small outlined text @click="e6 = 3">Confirm and Complete</v-btn>
-          <v-btn small text>Cancel</v-btn>
-        </v-stepper-content>
-
-        <v-stepper-step
-          class="sponsor__timeline-step"
-          color="#ae92b1"
-          :complete="e6 > 3"
-          step="3"
-          non-linear
-          >Finish transaction</v-stepper-step
-        >
-
-        <v-stepper-content step="3">
-          <v-card elevation="0" color="grey lighten-3" class="mb-3" height="50px"></v-card>
-          <v-btn small outlined text @click="e6 = 4">Complete Transaction</v-btn>
-          <v-btn small text>Cancel</v-btn>
-        </v-stepper-content>
-
-        <!-- <v-stepper-step step="4">View setup instructions</v-stepper-step>
-        <v-stepper-content step="4">
-          <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
-          <v-btn color="primary" @click="e6 = 1">Continue</v-btn>
-          <v-btn text>Cancel</v-btn>
-        </v-stepper-content> -->
-      </v-stepper>
-    </div>
-  </div>
+  </ValidationObserver>
 </template>
+
+<script lang="ts">
+import { ref } from '@vue/composition-api';
+
+export default {
+  name: 'Payment',
+  setup(props, { emit }) {
+    const sponsorName = ref('');
+
+    function emitSaveID() {
+      emit('SaveID', 'ID Saved');
+    }
+    return { emitSaveID, sponsorName };
+  }
+};
+</script>
 
 <script>
 export default {
--- src/views/Portfolio/views/index.ts
+++ src/views/Portfolio/views/index.ts
@@ -1,8 +1,8 @@
 import MyPrograms from './MyPrograms.vue';
-import CitizenID from './CitizenID.vue';
+import { CitizenID } from './CitizenID';
 import Settings from './Settings.vue';
 import Balance from './Balance.vue';
-import Referral from './Referral.vue';
+import Referral from './Referral';
 import Payment from './Payment.vue';
 
 export default {
