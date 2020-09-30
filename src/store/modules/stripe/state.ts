58cf5304759d22c070a2e1c9ba0de49b4e83b2ce
juxsta <ericjrreyes2@gmail.com>
2020-09-30 16:57:31 -0700

    :fire:fixed stripe

--- src/store/modules/stripe/state.ts
+++ src/store/modules/stripe/state.ts
@@ -2,7 +2,7 @@ import { loadStripe } from '@stripe/stripe-js';
 
 const state = {
   stripePromise: loadStripe(
-    'pk_test_51HLdB4LnkQGEgDQnafDjpychGkVt5DxQFpiZR1hDvLyPDkfsH8PrxmbyCy2G5ZJDPfKjR7GKt1SR4ylbiyehXuma00KrfKoSFC',
+    'pk_live_51HLdB4LnkQGEgDQnXGptKroG5ZA55RvdG1jpcnVPMsIhtGhzEaiuyozyHOXQs6UvJQ3A23cPmszUyMByLhXuUX7i00YVOZNJTl',
     { apiVersion: '2020-08-27' }
   )
 };
